8a1a789f27e80f488f2a6aebd3123741b24d4bbb
Grabemonster <example@example.net>
2024-11-15 18:30:27 +0100

    NixOS rebuild: Generation 419

--- modules/home-manager/yazi.nix
+++ modules/home-manager/yazi.nix
@@ -2,5 +2,16 @@
 {
   programs.yazi = {
     enable = true;
+    settings = {
+      keymap = {
+
+      };
+      theme = {};
+      yazi = {
+        manager = {
+          ratio = [1 4 4];
+        };
+      };
+    };
   };
 }
