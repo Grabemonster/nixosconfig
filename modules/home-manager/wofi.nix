cfebeb97708367a2359ba16772030864cbe9b88b
Grabemonster <example@example.net>
2024-12-02 19:57:29 +0100

    NixOS rebuild: Generation 479

--- modules/home-manager/wofi.nix
+++ modules/home-manager/wofi.nix
@@ -18,7 +18,7 @@
   home.file.".config/scripts/wofi-launcher.sh".text = ''
 #!${pkgs.bash}/bin/bash
     
-QUERY=$(wofi --dmenu --prompt "Eingabe")
+QUERY=$(wofi --drun --prompt "Eingabe")
 
 if [[ -z "$QUERY" ]]; then
   exit 0
