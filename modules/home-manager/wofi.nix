2f40000107272b34ff06510d233df62402b949b3
Grabemonster <example@example.net>
2024-12-02 21:42:59 +0100

    NixOS rebuild: Generation 497

--- modules/home-manager/wofi.nix
+++ modules/home-manager/wofi.nix
@@ -19,43 +19,5 @@
       height = "30%";
     };
     inherit style;
-  };
-
-  # Aktiviert Desktop-Dateien und Skripte
-  home.packages = [ pkgs.libnotify ];
-
-  # Skript erstellen
-  home.file."${config.home.homeDirectory}/.local/bin/calculate-handler.sh" = {
-    text = ''
-      #!/usr/bin/env bash
-
-      QUERY="$1"
-
-      if [[ "$QUERY" == "="* ]]; then
-        INPUT="${QUERY:1}"
-        RESULT=$(echo "$INPUT" | ${pkgs.bc}/bin/bc -l 2>/dev/null)
-        if [[ $? -ne 0 ]]; then
-          notify-send "Fehler" "Ungültiger Ausdruck: $INPUT"
-        else
-          notify-send "Ergebnis" "$INPUT = $RESULT"
-        fi
-      else
-        xdg-open "https://www.google.com/search?q=$QUERY"
-      fi
-    '';
-   
-  };
-
-  # .desktop-Datei erstellen
-  home.file."${config.home.homeDirectory}/.local/share/applications/calculator-handler.desktop" = {
-    text = ''
-      [Desktop Entry]
-      Name=Calculator
-      Comment=Perform quick calculations
-      Exec=${config.home.homeDirectory}/.local/bin/calculate-handler.sh %u
-      Terminal=false
-      Type=Application
-      Categories=Utility;
-    '';
-  };
+  }; 
 }
