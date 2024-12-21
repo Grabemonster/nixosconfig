b112bf419354a35e74608ec377ef41afec9ff3ea
Grabemonster <example@example.net>
2024-12-21 23:25:22 +0100

    NixOS rebuild: Generation 54

--- modules/home-manager/nvim/plugins/plugins.nix
+++ modules/home-manager/nvim/plugins/plugins.nix
@@ -1,28 +1,18 @@
 {pkgs, ...}: {
 imports = [
   ./lsp.nix
+  ./telescope.nix
+  ./cmp.nix
+  ./treesitter.nix
 ];
 programs.nixvim = { 
  plugins = { 
-    # Includes all parsers for treesitter
-    treesitter = {
-      enable = true;
-    };
-
+    
     # Icons 
     web-devicons.enable = true;
 
   
-    # Good old Telescope
-    telescope = {
-      enable = true;
-      extensions = {
-        fzf-native = {
-          enable = true;
-        };
-      };
-    };
-
+   
     # Todo comments
     todo-comments = {
       enable = true;
@@ -44,85 +34,10 @@ programs.nixvim = {
         focusOnToggle = true;
       };
     };
-
-   
-    
-    cmp-emoji = {
-      enable = true;
-    };
-
-    cmp = {
-      enable = true;
-      settings = {
-        completion = {
-          completeopt = "menu,menuone,noinsert";
-        };
-        autoEnableSources = true;
-        experimental = { ghost_text = true; };
-        performance = {
-          debounce = 60;
-          fetchingTimeout = 200;
-          maxViewEntries = 30;
-        };
-        
-        formatting = { fields = [ "kind" "abbr" "menu" ]; };
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "emoji"; }
-          {
-            name = "buffer"; # text within current buffer
-            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-            keywordLength = 3;
-          }
-          {
-            name = "path"; # file system paths
-            keywordLength = 3;
-          }
-          
-        ];
-
-        window = {
-          completion = { border = "solid"; };
-          documentation = { border = "solid"; };
-        };
-
-        mapping = {
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
-        };
-      };
-    };
-    cmp-nvim-lsp = {
-      enable = true; # LSP
-    };
-    cmp-buffer = {
-      enable = true;
-    };
-    cmp-path = {
-      enable = true; # file system paths
-    };
-    cmp-cmdline = {
-      enable = true; # autocomplete for cmdline
-    };  
   };
-
-
-  extraConfigLuaPre = ''
-    if vim.g.have_nerd_font then
-      require('nvim-web-devicons').setup {}
-    end
-  '';
+ 
 
   extraConfigLua = ''
-    require("telescope")
-
     kind_icons = {
       Text = "󰊄",
       Method = "",
@@ -153,6 +68,6 @@ programs.nixvim = {
 
     local cmp = require'cmp'
   ''; 
-};
+  };
 }
 
---
+++ modules/home-manager/nvim/plugins/telescope.nix
@@ -0,0 +1,13 @@
+{...}:
+{
+  programs.nixvim.plugins = {
+    telescope = {
+      enable = true;
+      extensions = {
+        fzf-native = {
+          enable = true;
+        };
+      };
+    };
+  };
+}
