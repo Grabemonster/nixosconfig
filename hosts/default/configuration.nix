20bcba72903336947696a7caba7683405d5153f4
Grabemonster <example@example.net>
2024-12-21 11:59:45 +0100

    NixOS rebuild: Generation 44

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -93,7 +93,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     gnome-terminal
-    wlroots
     wallust
     nixd
     lua-language-server
