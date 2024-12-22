f3d28af887cef6c1aee2613222ceb99a1ae5ea5a
Grabemonster <example@example.net>
2024-12-22 19:22:22 +0100

    NixOS rebuild: Generation 77

--- modules/home-manager/nvim/plugins/cmp.nix
+++ modules/home-manager/nvim/plugins/cmp.nix
@@ -42,12 +42,10 @@
 
         mapping = {
           "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
           "<S-Tab>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
+          "<Esc>" = "cmp.mapping.abort()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
           "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
         };
