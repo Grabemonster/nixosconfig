29672cf0ffc93fdb709270097722c01a346aa17f
Grabemonster <example@example.net>
2024-12-23 21:34:22 +0100

    NixOS rebuild: Generation 158

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -33,29 +33,22 @@
         extraConfig = ''
             set -g status-position top
 
-# Vim-Check
-set -g @is_vim "ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+            is_vim="ps -o state= -o comm= -t '#{pane_tty}' \ | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+            bind-key -n 'C-Left' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+            bind-key -n 'C-Down' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+            bind-key -n 'C-Up' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+            bind-key -n 'C-Right' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+                tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+            if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+                "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+            if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+                "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
 
-# Globale Tastenbindungen
-bind-key -n C-Left if-shell "$is_vim" "send-keys C-h" "select-pane -L"
-bind-key -n C-Down if-shell "$is_vim" "send-keys C-j" "select-pane -D"
-bind-key -n C-Up if-shell "$is_vim" "send-keys C-k" "select-pane -U"
-bind-key -n C-Right if-shell "$is_vim" "send-keys C-l" "select-pane -R"
-
-# Escape für C-\
-if -b '[#{version} < 3.0]' {
-    bind-key -n C-\\ if-shell "$is_vim" "send-keys C-\\\\" "select-pane -l"
-} {
-    bind-key -n C-\\ if-shell "$is_vim" "send-keys C-\\\\" "select-pane -l"
-}
-
-# Copy-Mode Bindings (optional, falls benötigt)
-bind-key -T copy-mode-vi C-h select-pane -L
-bind-key -T copy-mode-vi C-j select-pane -D
-bind-key -T copy-mode-vi C-k select-pane -U
-bind-key -T copy-mode-vi C-l select-pane -R
-bind-key -T copy-mode-vi C-\\ select-pane -l
-            
+            bind-key -T copy-mode-vi C-h select-pane -L
+            bind-key -T copy-mode-vi C-j select-pane -D
+            bind-key -T copy-mode-vi C-k select-pane -U
+            bind-key -T copy-mode-vi C-l select-pane -R
+            bind-key -T copy-mode-vi C-\\ select-pane -l
         '';
     };
 
