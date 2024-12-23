400d54ceff960a5332392fcdc5fda0e4a4c0dd94
Grabemonster <example@example.net>
2024-12-23 21:32:29 +0100

    NixOS rebuild: Generation 157

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -15,16 +15,6 @@
 
         plugins = with pkgs; [
             tmuxPlugins.sensible
-            {
-                plugin = tmuxPlugins.vim-tmux-navigator;
-                extraConfig = ''                    
-                    set -g @vim_navigator_mapping_left "C-LEFT"
-                    set -g @vim_navigator_mapping_right "C-RIGHT"
-                    set -g @vim_navigator_mapping_up "C-UP"
-                    set -g @vim_navigator_mapping_down "C-DOWN"
-                    set -g @vim_navigator_mapping_prev "C-p" 
-                '';
-            }
         tmuxPlugins.yank
         {
             plugin = tmuxPlugins.dracula;
@@ -42,6 +32,30 @@
 
         extraConfig = ''
             set -g status-position top
+
+# Vim-Check
+set -g @is_vim "ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+
+# Globale Tastenbindungen
+bind-key -n C-Left if-shell "$is_vim" "send-keys C-h" "select-pane -L"
+bind-key -n C-Down if-shell "$is_vim" "send-keys C-j" "select-pane -D"
+bind-key -n C-Up if-shell "$is_vim" "send-keys C-k" "select-pane -U"
+bind-key -n C-Right if-shell "$is_vim" "send-keys C-l" "select-pane -R"
+
+# Escape für C-\
+if -b '[#{version} < 3.0]' {
+    bind-key -n C-\\ if-shell "$is_vim" "send-keys C-\\\\" "select-pane -l"
+} {
+    bind-key -n C-\\ if-shell "$is_vim" "send-keys C-\\\\" "select-pane -l"
+}
+
+# Copy-Mode Bindings (optional, falls benötigt)
+bind-key -T copy-mode-vi C-h select-pane -L
+bind-key -T copy-mode-vi C-j select-pane -D
+bind-key -T copy-mode-vi C-k select-pane -U
+bind-key -T copy-mode-vi C-l select-pane -R
+bind-key -T copy-mode-vi C-\\ select-pane -l
+            
         '';
     };
 
