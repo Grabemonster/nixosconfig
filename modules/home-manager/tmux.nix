ab10ef6640159370498de92a8a30b21c05fd538a
Grabemonster <example@example.net>
2024-12-23 19:50:59 +0100

    NixOS rebuild: Generation 147

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -18,11 +18,11 @@
             {
                 plugin = tmuxPlugins.vim-tmux-navigator;
                 extraConfig = ''                    
-                    set -g @vim-tmux-navigator_mapping_left "C-Left C-h"
-                    set -g @vim-tmux-navigator_mapping_right "C-Right C-l"
-                    set -g @vim-tmux-navigator_mapping_up "C-Up C-k"
-                    set -g @vim-tmux-navigator_mapping_down "C-Down C-j"
-                    set -g @vim-tmux-navigator_mapping_prev "C-p" 
+                    set -g @vim_navigator_mapping_left "C-Left C-h"
+                    set -g @vim_navigator_mapping_right "C-Right C-l"
+                    set -g @vim_navigator_mapping_up "C-Up C-k"
+                    set -g @vim_navigator_mapping_down "C-Down C-j"
+                    set -g @vim_navigator_mapping_prev "C-p" 
                 '';
             }
         tmuxPlugins.yank
