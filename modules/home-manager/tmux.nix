7c610f77eb3de2823d7e80b9a8cdd3a646afbdc2
Grabemonster <example@example.net>
2024-12-23 21:57:04 +0100

    NixOS rebuild: Generation 164

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -52,7 +52,7 @@
     };
 
     home.file.".config/scripts/tmuxinatorselect".text = ''
-        selection=$(tmuxinator list | sed -n '2p' | tr ' ' '\n' | grep -v '^$' | wofi --no-sort --show dmenu | xargs)
+        selection=$(tmuxinator list | sed -n '2p' | tr ' ' '\n' | grep -v '^$' | wofi --sort-order=default --dmenu | xargs)
         if [ -n "$selection" ]; then
             gnome-terminal -- bash -c "tmuxinator start $selection; exec bash"
         else
