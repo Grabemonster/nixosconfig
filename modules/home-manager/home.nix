652bb667d3e85ea5df11b9e9d6dcfa7d77d0fb7b
Grabemonster <example@example.net>
2024-12-24 10:43:02 +0100

    NixOS rebuild: Generation 168

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -19,11 +19,17 @@
             ./terminal/fastfetch.nix
 
             ./apps/webbrowser.nix
+            ./apps/E-mail.nix
         ];
 # Home Manager needs a bit of information about you and the paths it should
 # manage.
     home.username = "${user}";
     home.homeDirectory = "/home/${user}";
+
+
+    home.packages = with pkgs; [
+        prismlauncher
+    ];
 
 # This value determines the Home Manager release that your configuration is
 # compatible with. This helps avoid breakage when a new Home Manager release
