8de5265468856d5cc434d9ac34caeebcb43342df
Grabemonster <example@example.net>
2025-01-01 14:17:53 +0100

    NixOS rebuild: Generation 80

--- modules/home-manager/system/rofi.nix
+++ modules/home-manager/system/rofi.nix
@@ -5,9 +5,10 @@
         package = pkgs.rofi;
         plugins = [pkgs.rofi-calc pkgs.rofi-emoji];
         extraConfig = {
-            modi = "drun,calc,emoji,combi";
+            modi = "drun,calc,emoji";
             kb-primary-paste = "Control+V,Shift+Insert";
             kb-secondary-paste = "Control+v,Insert";
         };
+        font = "JETBRAINS MONO";
     };
 }
