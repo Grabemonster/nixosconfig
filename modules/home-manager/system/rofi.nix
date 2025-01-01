59b97722c3df202fae1318faeb046f00f60625ac
Grabemonster <example@example.net>
2025-01-01 14:02:39 +0100

    NixOS rebuild: Generation 77

--- modules/home-manager/system/rofi.nix
+++ modules/home-manager/system/rofi.nix
@@ -3,5 +3,10 @@
     programs.rofi = {
         enable = true;
         package = pkgs.rofi.override { plugins = [ pkgs.rofi-calc];};
+        extraConfig = {
+            modi = "drun,calc";
+            kb-primary-paste = "Control+V,Shift+Insert";
+            kb-secondary-paste = "Control+v,Insert";
+        };
     };
 }
