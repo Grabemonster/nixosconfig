6dbad7152bd68ab0c561190dc13abc51ab1d4414
Grabemonster <example@example.net>
2025-01-01 14:26:25 +0100

    NixOS rebuild: Generation 81

--- modules/home-manager/system/rofi.nix
+++ modules/home-manager/system/rofi.nix
@@ -1,4 +1,4 @@
-{pkgs, ghostty, system, ...}:
+{pkgs, config, ...}:
 {
     programs.rofi = {
         enable = true;
@@ -10,5 +10,32 @@
             kb-secondary-paste = "Control+v,Insert";
         };
         font = "JETBRAINS MONO";
+        theme = let
+# Use `mkLiteral` for string-like values that should show without
+# quotes, e.g.:
+# {
+#   foo = "abc"; => foo: "abc";
+#   bar = mkLiteral "abc"; => bar: abc;
+# };
+            inherit (config.lib.formats.rasi) mkLiteral;
+        in {
+            "*" = {
+                background-color = mkLiteral "#000000";
+                foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
+                border-color = mkLiteral "#FFFFFF";
+                width = 512;
+            };
+
+            "#inputbar" = {
+                children = map mkLiteral [ "prompt" "entry" ];
+            };
+
+            "#textbox-prompt-colon" = {
+                expand = false;
+                str = ":";
+                margin = mkLiteral "0px 0.3em 0em 0em";
+                text-color = mkLiteral "@foreground-color";
+            };
+        };
     };
 }
