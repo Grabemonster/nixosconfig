70fdc9f40317b0cb332451b34644ed3ad1ead7d5
Grabemonster <example@example.net>
2025-01-01 14:12:08 +0100

    NixOS rebuild: Generation 79

--- modules/home-manager/system/rofi.nix
+++ modules/home-manager/system/rofi.nix
@@ -2,9 +2,10 @@
 {
     programs.rofi = {
         enable = true;
-        package = pkgs.rofi.override { plugins = [ pkgs.rofi-calc];};
+        package = pkgs.rofi;
+        plugins = [pkgs.rofi-calc pkgs.rofi-emoji];
         extraConfig = {
-            modi = "drun,calc,combi";
+            modi = "drun,calc,emoji,combi";
             kb-primary-paste = "Control+V,Shift+Insert";
             kb-secondary-paste = "Control+v,Insert";
         };
