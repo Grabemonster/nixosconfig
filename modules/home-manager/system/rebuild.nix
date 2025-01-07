104ca6ecbd66eaea2d0a139de5681082dc0e28d2
Grabemonster <example@example.net>
2025-01-07 16:56:29 +0100

    NixOS rebuild: Generation 156

--- modules/home-manager/system/rebuild.nix
+++ modules/home-manager/system/rebuild.nix
@@ -1,24 +1,32 @@
 {pkgs, ...}:
 {
   home.file.".config/scripts/rebuild".text = ''
-    #!${pkgs.bash}/bin/bash
+      #!${pkgs.bash}/bin/bash
 
-    cd ~/.config/nixos/
-    
-    git add *
+      cd ~/.config/nixos/
 
-    if sudo nixos-rebuild switch --flake ~/.config/nixos#default; then 
+      git add *
+      if [["$1" == "-t"]]; then
+          if sudo nixos-rebuild switch --flake ~/.config/nixos#default; then
+              echo "erfolgreicher test build"
+          else
+              echo ""
+              echo "testbuild fehlgeschlagen"
+          fi
+      else
+          if sudo nixos-rebuild switch --flake ~/.config/nixos#default; then 
 
-      latest_gen=$(ls -l /nix/var/nix/profiles/system-* | grep -oE 'system-([0-9]+)' | grep -Eo '[0-9]+' | sort -n | tail -1)
+              latest_gen=$(ls -l /nix/var/nix/profiles/system-* | grep -oE 'system-([0-9]+)' | grep -Eo '[0-9]+' | sort -n | tail -1)
 
-      git commit -a -m "NixOS rebuild: Generation $latest_gen"
+              git commit -a -m "NixOS rebuild: Generation $latest_gen"
 
-      echo ""
-      echo "NixOS rebuild erfolgreich. Git commit erstellt mit Generation $latest_gen."
+              echo ""
+              echo "NixOS rebuild erfolgreich. Git commit erstellt mit Generation $latest_gen."
 
-    else
-      echo ""
-      echo "NixOS rebuild fehlgeschlagen"
-    fi
+          else
+              echo ""
+              echo "NixOS rebuild fehlgeschlagen"
+          fi
+      fi
   '';
 }
