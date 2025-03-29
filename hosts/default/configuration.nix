5239b515e7dbbebea057bbeeef048db73a98a3a2
Grabemonster <example@example.net>
2025-03-29 13:05:56 +0100

    NixOS rebuild: Generation 230

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -121,11 +121,13 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [pkgs.hplip];
   services.avahi = {
       enable = true;
       nssmdns4 = true;
       openFirewall = true;
   };
+  
 
 
   # Enable sound with pipewire.
--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -65,7 +65,6 @@
         psmisc
         htop
         weylus
-        hplip
     ];
 
     home.sessionVariables = {
