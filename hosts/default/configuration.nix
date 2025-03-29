91b9cd2b2db7f6780ba8fc28391c543dc88be7a9
Grabemonster <example@example.net>
2025-03-29 13:54:40 +0100

    NixOS rebuild: Generation 231

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -122,9 +122,21 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.printing.drivers = [pkgs.hplip];
+  
   services.avahi = {
       enable = true;
       nssmdns4 = true;
+      openFirewall = true;
+      publish = {
+          enable = true;
+          userServices = true;
+      };
+  };
+  services.printing = {
+      listenAddresses = [ "*:631" ];
+      allowFrom = [ "all" ];
+      browsing = true;
+      defaultShared = true;
       openFirewall = true;
   };
   
