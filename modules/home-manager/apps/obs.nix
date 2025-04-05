23a8f24bcc6ef3b54a8851f3727f436a396ee1bc
Grabemonster <example@example.net>
2025-04-06 00:19:35 +0200

    NixOS rebuild: Generation 279

--- modules/home-manager/apps/obs.nix
+++ modules/home-manager/apps/obs.nix
@@ -2,6 +2,7 @@
 let
 pactl = ''${pkgs.pulseaudio}/bin/pactl'';
 pw-link = ''${pkgs.pipewire}/bin/pw-link'';
+pw-cli = ''${pkgs.pipewire}/bin/pw-cli'';
 in
 {
     programs.obs-studio = {
@@ -9,10 +10,10 @@ in
     };
 
     home.file.".config/scripts/audio.sh".text = ''
-        ${pkgs.pipewire}/bin/pw-cli ls | tr '\n' ' ' | sed $'s/\\tid/\\n/g' | grep custom | awk '{print $1}' | tr ',' ' ' | while read id; do pw-cli destroy "$id"; done
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=custom_SA sink_properties=device.description=\"SaveAudio\"
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=custom_uSA sink_properties=device.description=\"unSaveAudio\"
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=custom_CO sink_properties=device.description=\"CombinedOutput\"
+        ${pw-cli} ls | tr '\n' ' ' | sed $'s/\\tid/\\n/g' | grep custom | awk '{print $1}' | tr ',' ' ' | while read id; do pw-cli destroy "$id"; done
+        ${pactl} load-module module-null-sink sink_name=custom_SA sink_properties=device.description=\"SaveAudio\"
+        ${pactl} load-module module-null-sink sink_name=custom_uSA sink_properties=device.description=\"unSaveAudio\"
+        ${pactl} load-module module-null-sink sink_name=custom_CO sink_properties=device.description=\"CombinedOutput\"
 
         ${pw-link} "SaveAudio:monitor_FL" "CombinedOutput:playback_FL"
         ${pw-link} "SaveAudio:monitor_FR" "CombinedOutput:playback_FR"
@@ -24,12 +25,12 @@ in
 
         
 # Hole alle Ausgabegeräte (außer dem custom Sink)
-        devices=$(pw-cli ls| tr '\n' ' '| sed $'s/\\tid/\\n\\n&/g' | grep -v custom | grep output | tr '\t' '\n' | grep node.name | awk -F '"' '{print $2}')
+        devices=$(${pw-cli} ls| tr '\n' ' '| sed $'s/\\tid/\\n\\n&/g' | grep -v custom | grep output | tr '\t' '\n' | grep node.name | awk -F '"' '{print $2}')
 
 # Verbinde jedes Gerät mit dem virtuellen Sink
         for device in $devices; do
             echo "Verbinde Gerät $device mit custom_CO"
-            pw-link "custom_CO" "$device" 
+            ${pw-link} "custom_CO" "$device" 
         done
 
     '';
