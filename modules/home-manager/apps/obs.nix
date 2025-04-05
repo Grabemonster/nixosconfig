0421c3a0ecb43d2b23bdb623961b4bf5393cce96
Grabemonster <example@example.net>
2025-04-05 21:22:33 +0200

    NixOS rebuild: Generation 242

--- modules/home-manager/apps/obs.nix
+++ modules/home-manager/apps/obs.nix
@@ -1,12 +1,12 @@
-{...}:
+{pkgs, ...}:
 {
     programs.obs-studio = {
         enable = true;
     };
 
     home.file.".config/scripts/audio.sh".text = ''
-        {pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"SaveAudio\"
-        {pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\
-        {pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"SaveAudio\"
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\
     '';
 }
