c79cdb336e00318810e1e592a2683f0519835e90
Grabemonster <example@example.net>
2025-04-05 22:03:36 +0200

    NixOS rebuild: Generation 243

--- modules/home-manager/apps/obs.nix
+++ modules/home-manager/apps/obs.nix
@@ -1,12 +1,16 @@
 {pkgs, ...}:
+let
+pactl = ''${pkgs.pulseaudio}/bin/pactl'';
+in
 {
     programs.obs-studio = {
         enable = true;
     };
 
     home.file.".config/scripts/audio.sh".text = ''
+        ${pkgs.pipewire}/bin/pw-cli ls | tr '\n' ' ' | sed $'s/\\tid/\\n/g' | grep CombinedOutput | awk '{print $1}' | tr ',' ' ' | while read id; do pw-cli destroy "$id"; done
         ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"SaveAudio\"
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\"
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\"
     '';
 }
