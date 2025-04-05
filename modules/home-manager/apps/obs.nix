185c63104a46bb35ef01286dfcb98ece25455b1d
Grabemonster <example@example.net>
2025-04-05 21:19:57 +0200

    NixOS rebuild: Generation 239

--- modules/home-manager/apps/obs.nix
+++ modules/home-manager/apps/obs.nix
@@ -3,4 +3,10 @@
     programs.obs-studio = {
         enable = true;
     };
+
+    home.file.".config/scripts/audio.sh".text = ''
+        {pkgs.pipewire}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"SaveAudio\"
+        {pkgs.pipewire}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\
+        {pkgs.pipewire}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\
+    '';
 }
