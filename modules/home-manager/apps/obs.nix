4ba0981a0a5dfbbc9752522c86e849ba7975e13a
Grabemonster <example@example.net>
2025-04-05 22:09:23 +0200

    NixOS rebuild: Generation 244

--- modules/home-manager/apps/obs.nix
+++ modules/home-manager/apps/obs.nix
@@ -8,9 +8,9 @@ in
     };
 
     home.file.".config/scripts/audio.sh".text = ''
-        ${pkgs.pipewire}/bin/pw-cli ls | tr '\n' ' ' | sed $'s/\\tid/\\n/g' | grep CombinedOutput | awk '{print $1}' | tr ',' ' ' | while read id; do pw-cli destroy "$id"; done
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"SaveAudio\"
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\"
-        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\"
+        ${pkgs.pipewire}/bin/pw-cli ls | tr '\n' ' ' | sed $'s/\\tid/\\n/g' | grep custom | awk '{print $1}' | tr ',' ' ' | while read id; do pw-cli destroy "$id"; done
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=custom sink_properties=device.description=\"SaveAudio\"
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=custom sink_properties=device.description=\"unSaveAudio\"
+        ${pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=custom sink_properties=device.description=\"CombinedOutput\"
     '';
 }
