fa660f02f5af88d33e823f15e22eb8e870436f30
Grabemonster <example@example.net>
2025-04-05 21:21:01 +0200

    NixOS rebuild: Generation 240

--- modules/home-manager/apps/obs.nix
+++ modules/home-manager/apps/obs.nix
@@ -5,8 +5,8 @@
     };
 
     home.file.".config/scripts/audio.sh".text = ''
-        {pkgs.pipewire}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"SaveAudio\"
-        {pkgs.pipewire}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\
-        {pkgs.pipewire}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\
+        {pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"SaveAudio\"
+        {pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"unSaveAudio\
+        {pkgs.pulseaudio}/bin/pactl load-module module-null-sink sink_name=CombinedOutput sink_properties=device.description=\"CombinedOutput\
     '';
 }
