4c72d6dd62aabb3ab43dc82657040e4d2470658a
Grabemonster <example@example.net>
2025-04-06 12:03:14 +0200

    NixOS rebuild: Generation 289

--- modules/home-manager/apps/webbrowser.nix
+++ modules/home-manager/apps/webbrowser.nix
@@ -8,7 +8,7 @@
     profiles."${user}" = {
       id = 0;
       isDefault = true;
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         bitwarden
         ublock-origin
       ];
