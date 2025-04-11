9b515f8dd7e88309b890241d34d2801e495a68c2
Grabemonster <example@example.net>
2025-04-11 14:36:52 +0200

    NixOS rebuild: Generation 297

--- modules/home-manager/apps/obs.nix
+++ modules/home-manager/apps/obs.nix
@@ -5,28 +5,40 @@ pw-link = ''${pkgs.pipewire}/bin/pw-link'';
 pw-cli = ''${pkgs.pipewire}/bin/pw-cli'';
 audioStartupScript = pkgs.writeShellScriptBin "audio-startup" ''
     #!${pkgs.bash}/bin/bash
-        ${pw-cli} ls | tr '\n' ' ' | sed $'s/\\tid/\\n/g' | grep custom | awk '{print $1}' | tr ',' ' ' | while read id; do pw-cli destroy "$id"; done
-        ${pactl} load-module module-null-sink sink_name=custom_SA sink_properties=device.description=\"SaveAudio\"
-        ${pactl} load-module module-null-sink sink_name=custom_uSA sink_properties=device.description=\"unSaveAudio\"
-        ${pactl} load-module module-null-sink sink_name=custom_CO sink_properties=device.description=\"CombinedOutput\"
+    ${pw-cli} ls | tr '\n' ' ' | sed $'s/\\tid/\\n/g' | grep custom | awk '{print $1}' | tr ',' ' ' | while read id; do pw-cli destroy "$id"; done
+    ${pactl} load-module module-null-sink sink_name=custom_SA sink_properties=device.description=\"SaveAudio\"
+    ${pactl} load-module module-null-sink sink_name=custom_uSA sink_properties=device.description=\"unSaveAudio\"
+    ${pactl} load-module module-null-sink sink_name=custom_CO sink_properties=device.description=\"CombinedOutput\"
 
-        ${pw-link} "SaveAudio:monitor_FL" "CombinedOutput:playback_FL"
-        ${pw-link} "SaveAudio:monitor_FR" "CombinedOutput:playback_FR"
+    ${pw-link} "SaveAudio:monitor_FL" "CombinedOutput:playback_FL"
+    ${pw-link} "SaveAudio:monitor_FR" "CombinedOutput:playback_FR"
 
-        ${pw-link} "unSaveAudio:monitor_FL" "CombinedOutput:playback_FL"
-        ${pw-link} "unSaveAudio:monitor_FR" "CombinedOutput:playback_FR"
-        ${pactl} set-default-sink customuSA 
-# Hole alle Ausgabegeräte (außer dem custom Sink)
-        devices=$(${pw-cli} ls| tr '\n' ' '| sed $'s/\\tid/\\n\\n&/g' | grep -v custom | grep output | tr '\t' '\n' | grep node.name | awk -F '"' '{print $2}')
+    ${pw-link} "unSaveAudio:monitor_FL" "CombinedOutput:playback_FL"
+    ${pw-link} "unSaveAudio:monitor_FR" "CombinedOutput:playback_FR"
+    ${pactl} set-default-sink custom_uSA 
 
-# Verbinde jedes Gerät mit dem virtuellen Sink
-        for device in $devices; do
-            echo "Verbinde Gerät $device mit custom_CO"
-            ${pw-link} "custom_CO" "$device" 
-        done
+    ${updateOutputDevices}/bin/device-update
 '';
-in
-{
+
+updateOutputDevices = pkgs.writeShellScriptBin "device-update" ''
+    devices=$(${pw-cli} ls| tr '\n' ' '| sed $'s/\\tid/\\n\\n&/g' | grep -v custom | grep output | tr '\t' '\n' | grep node.name | awk -F '"' '{print $2}')
+    for device in $devices; do
+        echo "Verbinde Gerät $device mit custom_CO"
+        ${pw-link} "custom_CO" "$device" 
+    done
+'';
+
+
+autoLinkDaemon = pkgs.writeShellScriptBin "auto-link-daemon" ''
+    #!${pkgs.bash}/bin/bash
+    ${pkgs.pipewire}/bin/pw-mon | while read -r line; do
+        if echo "$line" | grep -q "new device"; then
+            echo "Neues Gerät erkannt – Verbinde..."
+            ${updateOutputDevices}/bin/device-update
+        fi
+    done
+  '';
+in{
 
 
     systemd.user.services.audioStartup = {
@@ -42,4 +54,18 @@ in
             WantedBy = [ "default.target" ];
         };
     }; 
+
+    systemd.user.services.audioAutoLink = {
+    Unit = {
+      Description = "Auto-link new PipeWire devices to CombinedOutput";
+      After = [ "pipewire.service" ];
+    };
+    Service = {
+      ExecStart = "${autoLinkDaemon}/bin/auto-link-daemon";
+      Restart = "always";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
