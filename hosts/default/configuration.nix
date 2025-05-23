ad1836cf76c943f6248566754a0bc4b6343dd1d3
Grabemonster <example@example.net>
2025-05-23 15:43:43 +0200

    NixOS rebuild: Generation 425

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -5,242 +5,242 @@
 {config, system, user, ghostty, pkgs, inputs, nur, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-    ];
+    imports =
+        [ # Include the results of the hardware scan.
+            ./hardware-configuration.nix
+            inputs.home-manager.nixosModules.default
+        ];
 
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
+    boot.loader = {
+        efi = {
+            canTouchEfiVariables = true;
+        };
+        grub = {
+            efiSupport = true;
+            device = "nodev";
+            useOSProber = true;
+        }; 
     };
-    grub = {
-      efiSupport = true;
-      device = "nodev";
-      useOSProber = true;
+
+    fileSystems."/srv/games" = {
+        device = "/dev/disk/by-uuid/cbce87f7-2044-4362-ab81-e7a745a3a4af";
+        fsType = "ext4";
+        options = [
+            "defaults"
+            "rw"
+        ];
+    };
+    #swapDevices = [
+    #{ device = "/dev/disk/by-uuid/e406c7ec-bdb9-4117-8e38-f07235d25fd9"; }
+    #];
+
+    networking.hostName = "nixos"; # Define your hostname.
+
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.settings.trusted-users = [ "root" "grabemonster" ];
+
+    # Enable OpenGL
+    hardware.graphics = {
+        enable = true;
+    };
+
+    # Load nvidia driver for Xorg and Wayland
+    services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+
+        # Modesetting is required.
+        modesetting.enable = true;
+
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+        # of just the bare essentials.
+        powerManagement.enable = true;
+
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
+
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of 
+        # supported GPUs is at: 
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        # Only available from driver 515.43.04+
+        # Currently alpha-quality/buggy, so false is currently the recommended setting.
+        open = false;
+
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
+
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+
+
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "de_DE.UTF-8";
+        LC_IDENTIFICATION = "de_DE.UTF-8";
+        LC_MEASUREMENT = "de_DE.UTF-8";
+        LC_MONETARY = "de_DE.UTF-8";
+        LC_NAME = "de_DE.UTF-8";
+        LC_NUMERIC = "de_DE.UTF-8";
+        LC_PAPER = "de_DE.UTF-8";
+        LC_TELEPHONE = "de_DE.UTF-8";
+        LC_TIME = "de_DE.UTF-8";
+    };
+
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "de";
+        variant = "";
+    };
+
+    # Configure console keymap
+    console.keyMap = "de";
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+    services.printing.drivers = [pkgs.hplip];
+
+    services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+        publish = {
+            enable = true;
+            userServices = true;
+        };
+    };
+
+
+
+    # Enable sound with pipewire.
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
+    xdg.portal = {
+        enable = true;
+        xdgOpenUsePortal = true;
+        extraPortals = [
+            pkgs.xdg-desktop-portal-hyprland
+            pkgs.xdg-desktop-portal-gnome
+            pkgs.xdg-desktop-portal-gtk 
+            pkgs.xdg-desktop-portal-wlr
+        ];
+    };
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.${user} = {
+        isNormalUser = true;
+        description = "${user}";
+        shell = "${pkgs.zsh}/bin/zsh";
+        extraGroups = [ "networkmanager" "wheel" "dialout" "${user}"];
+    };
+
+    home-manager = {
+        extraSpecialArgs = {inherit inputs pkgs user nur system ghostty; };
+        users.${user} = import ./../../modules/home-manager/home.nix;
+    };
+
+    programs.hyprland.enable = true;
+
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     }; 
-  };
 
-  fileSystems."/srv/games" = {
-    device = "/dev/disk/by-uuid/cbce87f7-2044-4362-ab81-e7a745a3a4af";
-    fsType = "ext4";
-    options = [
-        "defaults"
-        "rw"
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        vim 
+        wget
+        gnome-terminal
+        pkgs.hplip
+        helvum
+        pulseaudio
     ];
-  };
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/e406c7ec-bdb9-4117-8e38-f07235d25fd9"; }
-  ];
 
-  networking.hostName = "nixos"; # Define your hostname.
+    programs.obs-studio = {
+        enable = true;
+        plugins = with pkgs.obs-studio-plugins; [
+            obs-backgroundremoval 
+        ];
+        enableVirtualCamera = true;
+    };
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nix.settings.trusted-users = [ "root" "grabemonster" ];
-
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-
-
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "de";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = [pkgs.hplip];
-  
-  services.avahi = {
-      enable = true;
-      nssmdns4 = true;
-      openFirewall = true;
-      publish = {
-          enable = true;
-          userServices = true;
-      };
-  };
-  
-
-
-  # Enable sound with pipewire.
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    extraPortals = [
-        pkgs.xdg-desktop-portal-hyprland
-        pkgs.xdg-desktop-portal-gnome
-        pkgs.xdg-desktop-portal-gtk 
-        pkgs.xdg-desktop-portal-wlr
+    fonts.packages = with pkgs; [ 
+        dejavu_fonts
+        font-awesome
+        jetbrains-mono
+        nerd-fonts.droid-sans-mono
     ];
-  };
- 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${user} = {
-    isNormalUser = true;
-    description = "${user}";
-    shell = "${pkgs.zsh}/bin/zsh";
-    extraGroups = [ "networkmanager" "wheel" "dialout" "${user}"];
-  };
 
-  home-manager = {
-    extraSpecialArgs = {inherit inputs pkgs user nur system ghostty; };
-    users.${user} = import ./../../modules/home-manager/home.nix;
-  };
-
-   programs.hyprland.enable = true;
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  }; 
- 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim 
-    wget
-    gnome-terminal
-    pkgs.hplip
-    helvum
-    pulseaudio
-  ];
-
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      obs-backgroundremoval 
-    ];
-    enableVirtualCamera = true;
-  };
-
-  fonts.packages = with pkgs; [ 
-    dejavu_fonts
-    font-awesome
-    jetbrains-mono
-    nerd-fonts.droid-sans-mono
-  ];
-
-  nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}"];
+    nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}"];
 
 
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = 1;
-  };
-  environment.variables = {
-    MOZ_ENABLE_WAYLAND = "1"; 
-  };
-  environment.pathsToLink = ["/share/zsh"];
-  # programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    environment.sessionVariables = {
+        NIXOS_OZONE_WL = 1;
+    };
+    environment.variables = {
+        MOZ_ENABLE_WAYLAND = "1"; 
+    };
+    environment.pathsToLink = ["/share/zsh"];
+    # programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-  # List services that you want to enable:
+    # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 1701 9001 ];
-  networking.firewall.allowedUDPPorts = [ ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # Open ports in the firewall.
+    networking.firewall.allowedTCPPorts = [ 1701 9001 ];
+    networking.firewall.allowedUDPPorts = [ ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "24.05"; # Did you read the comment?
 
 }
