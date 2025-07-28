30e1cddb1edd49b75fd9d2d962134a4e859d2611
Grabemonster <example@example.net>
2025-07-28 18:25:35 +0200

    NixOS rebuild: Generation 489

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -196,6 +196,9 @@
 
     programs.obs-studio = {
         enable = true;
+        package = (pkgs.obs-studio.override {
+            cudaSupport = true;
+        });
         plugins = with pkgs.obs-studio-plugins; [
             obs-backgroundremoval 
         ];
