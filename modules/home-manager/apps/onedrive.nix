4b079ec5e2f05ce8ed6e5f5353ed9390c453e8f2
Grabemonster <example@example.net>
2025-09-06 11:52:15 +0200

    NixOS rebuild: Generation 499

--- modules/home-manager/apps/onedrive.nix
+++ modules/home-manager/apps/onedrive.nix
@@ -1,4 +1,4 @@
-{...}:
+{pkgs, config, ...}:
 {
     programs.onedrive = {
         enable = true;
@@ -22,4 +22,35 @@
             sync_dir = "/srv/games/Onedrive";
         };
     };
+
+    systemd.user.services.rclone-onedrive = {
+    Unit = {
+      Description = "Rclone OneDrive Mount";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+
+    Service = {
+      ExecStart = ''
+        ${pkgs.rclone}/bin/rclone mount \
+          onedrive:Backup \
+          ${config.home.homeDirectory}/OneDriveBackup \
+          --vfs-cache-mode=full \
+          --vfs-cache-max-size 2G \
+          --vfs-cache-max-age 12h \
+          --dir-cache-time 12h \
+          --poll-interval 15s \
+          --umask 022 \
+          --allow-other \
+          --daemon
+      '';
+      ExecStop = "${pkgs.fuse}/bin/fusermount -u ${config.home.homeDirectory}/OneDrive";
+      Restart = "on-failure";
+      RestartSec = 10;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
