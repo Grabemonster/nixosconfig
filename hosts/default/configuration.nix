9542bc59d55c1005bc7ea94839fab866588a90f6
Grabemonster <example@example.net>
2025-09-06 16:41:10 +0200

    NixOS rebuild: Generation 544

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -192,7 +192,18 @@
         helvum
         pulseaudio
         easyeffects
+        libsForQt5.qtstyleplugin-kvantum
+        libsForQt5.qt5ct
     ];
+
+    nixpkgs.config.qt5 = {
+        enable = true;
+        platformTheme = "qt5ct"; 
+        style = {
+            package = pkgs.utterly-nord-plasma;
+            name = "Utterly Nord Plasma";
+        };
+    };
 
     programs.obs-studio = {
         enable = true;
