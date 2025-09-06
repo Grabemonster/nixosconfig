516a00fd9999e7f1d5752d08d1d6a0f4b0096dd2
Grabemonster <example@example.net>
2025-09-06 17:24:00 +0200

    NixOS rebuild: Generation 549

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -110,8 +110,8 @@
         enable=true;
         platformTheme.name = "qt5ct";
         style = {
-            name="adwaita-dark";
-            package=pkgs.adwaita-qt;
+            name="bb10dark";
+            package=pkgs.libsForQt5.qtstyleplugins;
         };
     };
 
