63b7b5f4d12f775bd2840a4f5853b428b04f9412
Grabemonster <example@example.net>
2025-09-06 16:13:59 +0200

    NixOS rebuild: Generation 537

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -108,32 +108,11 @@
 
     
     
-    xdg.configFile = {
-        kvantum = {
-            target = "Kvantum/kvantum.kvconfig";
-            text = lib.generators.toINI { } {
-                General.theme = "Catppuccin-Mocha-Blue";
-            };
-        };
-
-        qt5ct = {
-            target = "qt5ct/qt5ct.conf";
-            text = lib.generators.toINI { } {
-                Appearance = {
-                    icon_theme = "Papirus-Dark";
-                };
-            };
-        };
-
-        qt6ct = {
-            target = "qt6ct/qt6ct.conf";
-            text = lib.generators.toINI { } {
-                Appearance = {
-                    icon_theme = "Papirus-Dark";
-                };
-            };
-        };
-    };
+    qt = {
+    enable = true;
+    platformTheme.name = "qtct";
+    style.name = "kvantum";
+  };
 
 
 
