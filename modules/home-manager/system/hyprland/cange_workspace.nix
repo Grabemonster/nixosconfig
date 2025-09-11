4871993a333c15f2859e9cf85a7c0ffda4026aa7
Grabemonster <example@example.net>
2025-09-11 15:33:04 +0200

    NixOS rebuild: Generation 562

--- modules/home-manager/system/hyprland/cange_workspace.nix
+++ modules/home-manager/system/hyprland/cange_workspace.nix
@@ -3,14 +3,14 @@
     home.file.".config/scripts/workspace.sh".text = ''
         #!${pkgs.bash}/bin/bash
 
-        firstnumber=$(hyprctl activeworkspace | grep -Eo " [0-9]+ " | grep -Eo "[0-9]+" | grep -oP "^.*(?=.$)")       
+        local firstnumber=$(hyprctl activeworkspace | awk '{print $3}' | xargs | cut -c 1)       
         hyprctl dispatch workspace $firstnumber$1
     '';
 
     home.file.".config/scripts/movetoworkspace.sh".text = ''
         #!${pkgs.bash}/bin/bash
 
-        firstnumber=$(hyprctl activeworkspace | grep -Eo " [0-9]+ " | grep -Eo "[0-9]+" | grep -oP "^.*(?=.$)")       
+        local firstnumber=$(hyprctl activeworkspace | awk '{print $3}' | xargs | cut -c 1)
         hyprctl dispatch movetoworkspace $firstnumber$1
     '';
 }
