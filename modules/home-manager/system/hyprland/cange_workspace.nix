9982984a75c27ac93bf75365aa5ff71ac21a3885
Grabemonster <example@example.net>
2025-09-11 15:52:52 +0200

    NixOS rebuild: Generation 567

--- modules/home-manager/system/hyprland/cange_workspace.nix
+++ modules/home-manager/system/hyprland/cange_workspace.nix
@@ -3,14 +3,14 @@
     home.file.".config/scripts/workspace.sh".text = ''
         #!${pkgs.bash}/bin/bash
 
-        firstnumber=$(${pkgs.hyprland}/bin/hyprctl activeworkspace -j | ${pkgs.jq}/bin/jq -r '.id' | awk '{ print substr( $0, 1, length($0)-1 ) }')       
+        firstnumber=$(${pkgs.hyprland}/bin/hyprctl activeworkspace -j | ${pkgs.jq}/bin/jq -r '.id' | xargs | awk '{ print substr( $0, 1, length($0)-1 ) }')       
         ${pkgs.hyprland}/bin/hyprctl dispatch movetoworkspace "firstnumber$1"
     '';
 
     home.file.".config/scripts/movetoworkspace.sh".text = ''
         #!${pkgs.bash}/bin/bash
 
-        firstnumber=$(${pkgs.hyprland}/bin/hyprctl activeworkspace -j | ${pkgs.jq}/bin/jq -r '.id' | awk '{ print substr( $0, 1, length($0)-1 ) }')
+        firstnumber=$(${pkgs.hyprland}/bin/hyprctl activeworkspace -j | ${pkgs.jq}/bin/jq -r '.id' | xargs | awk '{ print substr( $0, 1, length($0)-1 ) }')
         ${pkgs.hyprland}/bin/hyprctl dispatch movetoworkspace "firstnumber$1"
     '';
 }
