d0fe5e6701ce1af08186110ba175ae9eee092505
Grabemonster <example@example.net>
2025-09-30 16:46:12 +0200

    NixOS rebuild: Generation 580

--- modules/home-manager/terminal/tmux.nix
+++ modules/home-manager/terminal/tmux.nix
@@ -50,7 +50,7 @@
         #!${pkgs.bash}/bin/bash
         selection=$(tmuxinator list | tail -n +2 | tr ' ' '\n' | grep -v '^$' | rofi -sort-order=default -dmenu)
         if [ -n "$selection" ]; then
-            ghostty -e "tmuxinator start $selection; exec zsh"
+            ghostty -e "tmuxinator start $selection"
         else
             echo "Keine Auswahl getroffen." >&2
         fi
