4a98dca8387626e2cab1c506957fd033cca95d98
Grabemonster <example@example.net>
2025-10-12 17:54:03 +0200

    NixOS rebuild: Generation 584

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -171,14 +171,6 @@
 
     programs.hyprland.enable = true;
 
-    programs.virt-manager.enable = true;
-
-    users.groups.libvirtd.members = [user];
-
-    virtualisation.libvirtd.enable = true;
-
-    virtualisation.spiceUSBRedirection.enable = true;
-
 
     programs.steam = {
         enable = true;
